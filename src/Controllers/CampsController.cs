7b5fc6f04460c5fe29cd7a0cba2ceb93c47ddeb1
Ivan MSI <arigatory@gmail.com>
2021-12-06 00:25:04 +0300

    using status codes

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            return  Ok(new {Moiker = "ATL2018", Name = "Atlanta Code Camp" });
+        }
+    }
+}
